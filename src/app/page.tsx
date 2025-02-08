d3b63d3710c1ef19c78aac33a43a628f7097047f
amna-sheikh <amnaaptech@gmail.com>
2025-02-08 19:25:45 +0500

    admin panel
    admin panel

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { useUser ,SignedOut,SignInButton,SignOutButton} from "@clerk/nextjs";
+import { useRouter } from "next/router"
+
+  export default function(){
+    const {user} = useUser()
+    const router = useRouter()
+
+  // dashboard ka folder banane
+if(user &&user.primaryEmailAddress?.emailAddress !=='amnaadnan011gmail.com'){
+router.push("/dashboard") 
+}
+
+return(
+   <div className="flex justify-center items-center h-screen bg-gray-100 ">
+    <h1 className="">Admin LogIn</h1>
+<SignedOut>
+  <SignInButton>
+<button className="bg-[#2A254B] text-white px-4 py-2 rounded ">
+  LogIn With Clerk 
+</button>
+  </SignInButton>
+</SignedOut>
+
+<SignOutButton>
+<button className="bg-[#2A254B] text-white px-4 py-2 rounded ">
+  LogOut With Clerk 
+</button>
+</SignOutButton>
+
+  </div>
+)
+  }
